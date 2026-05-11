913efcc0e8b973a29d7e41749dfe5b6242e7919f
b9Mike <migu31.z.a@gmail.com>
2026-05-10 23:43:55 -0600

    feature: add update profile image and check authentication endpoint

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+import { ENV } from "../lib/env.js";
+import User from "../models/User.js"
+import jwt from "jsonwebtoken";
+
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+        if(!token) return res.status(401).json({ message: "Unauthorized  - No token provided" });
+
+        const decode = jwt.verify(token, ENV.JWT_SECRET);
+        if(!decode) return res.status(401).json({ message: "Unauthorized  - Invalid token" });
+
+        const user = await User.findById(decode.userId).select("-password");
+        if(!user) return res.status(404).json({ message: "User not found" });
+
+        req.user = user;
+        next();
+
+    } catch (error) {
+        console.error("Error in protectedRoute middleware: ", error);
+        res.status(500).json({ message: "Internal server error" });
+        
+    }
+}
