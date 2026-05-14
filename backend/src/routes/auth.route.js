0a0b96329b83714e7c218864a909d1d5183d6699
b9Mike <migu31.z.a@gmail.com>
2026-05-13 20:46:33 -0600

    feat: add rate limiting

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,19 @@
 import express from "express"
 import { login, logout, signup, updateProfile } from "../controllers/auth.contoller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection);
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.get("/test", (req, res) => {
+    return res.status(403).json({ message: "Test arcjet :D"});
+});
 
 router.put("/update-profile", protectRoute, updateProfile);
 
